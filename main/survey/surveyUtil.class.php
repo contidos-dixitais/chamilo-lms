95e225cde70b9ec013c058046443deb0e76b081e
juancpbinario <124381395+juancpbinario@users.noreply.github.com>
2024-01-16 23:05:54 +0100

    Funcionalidad para poder indicar código de encuesta al clonar una encuesta

--- main/survey/surveyUtil.class.php
+++ main/survey/surveyUtil.class.php
@@ -3331,6 +3331,26 @@ class SurveyUtil
         $formToString = $form->returnForm();
 
         echo '<div id="dialog-confirm">'.$formToString.'</div>';
+
+        $form = new FormValidator(
+            'copy-survey',
+            'post',
+            null,
+            null,
+            ['class' => 'form-vertical']
+        );
+        $form->addElement(
+            'text',
+            'survey_code',
+            get_lang('SurveyCode'),
+            ['size' => 20, 'maxlength' => 20]
+        );
+
+        //$form->setDefaults(['survey_code' => '']);
+        $formToString = $form->returnForm();
+
+        echo '<div id="dialog-copy-confirm">'.$formToString.'</div>';
+
         $table->display();
     }
 
@@ -3417,7 +3437,7 @@ class SurveyUtil
      *
      * @version January 2007
      */
-    public static function modify_filter($survey_id, $drh = false)
+    public static function modify_filter($survey_id, $drh = false, $surveyCode = "")
     {
         /** @var CSurvey $survey */
         $survey = Database::getManager()->find('ChamiloCourseBundle:CSurvey', $survey_id);
@@ -3485,7 +3505,8 @@ class SurveyUtil
                 $actions[] = Display::url(
                     Display::return_icon('copy.png', get_lang('DuplicateSurvey')),
                     $codePath.'survey/survey_list.php?'
-                    .http_build_query($params + ['action' => 'copy_survey', 'survey_id' => $survey_id])
+                    .http_build_query($params + ['action' => 'copy_survey', 'survey_id' => $survey_id]),
+                    ['survey_id' => $survey_id, 'class' => 'copy_survey_popup']
                 );
 
                 $actions[] = Display::url(
