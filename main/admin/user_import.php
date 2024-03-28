fe1369fc0b73531ce1adcb84b976a47458dcddce
juancpbinario <juan.cortizas@almabinaria.com>
2024-03-28 21:25:39 +0100

    Integrar validación campos extra usuario en importación masiva

--- main/admin/user_import.php
+++ main/admin/user_import.php
@@ -197,6 +197,21 @@ function validate_data($users, $checkUniqueEmail = false)
                 $user['has_error'] = true;
             }
         }
+
+        // 6. Check if extra fields are duplicated
+        $extraFields = api_get_configuration_value('registration_extra_fields_to_check');
+        if (!empty($extraFields) && isset($extraFields['extra_fields'])) {
+            $extraFieldList = $extraFields['extra_fields'];
+            foreach ($extraFieldList as $extraFieldToCheck) {
+                if (isset($user[$extraFieldToCheck])) {
+                    $valueExists = api_extra_field_validation($extraFieldToCheck, $user[$extraFieldToCheck]);
+                    if ($valueExists) {
+                        $user['message'] .= Display::return_message(get_lang('DuplicatedFieldAt').' '.$extraFieldToCheck, 'warning');
+                        $user['has_error'] = true;
+                    }
+                }
+            }
+        }
     }
 
     return $users;
